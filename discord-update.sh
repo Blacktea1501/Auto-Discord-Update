ecbda3847a431fc75f7c0b201f039377ba7490c8
Artur <88030694+Blacktea1501@users.noreply.github.com>
2022-01-20 12:13:19 +0100

    Add files via upload

---
+++ discord-update.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+url="https://discord.com/api/download?platform=linux&format=deb"
+name="Discord"
+tmpname="discord.deb"
+clear;
+
+echo Killing all processes with name $name.;
+
+#killing all processes with the name Discord
+
+for KILLPID in `ps ax | grep $name | awk ' { print $1;}'`; do
+ kill -9 $KILLPID &> /dev/null
+done
+
+echo
+echo Pulling $tmpname from $url;
+
+wget -O $tmpname $url
+
+
+echo
+echo Installing $tmpname;
+
+
+# installing discord with all dependencies
+
+sudo apt-get install ./$tmpname
+
+# removing the temp .deb file
+
+rm $tmpname
+sudo apt-get autoremove
+
+echo
+echo Finished;
+echo You can now launch Discord;
+
+
+
